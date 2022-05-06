abf2cd947782464e8b0f0ca6705c4e4687d71450
Gabrielle Befort <mrsbefort@gmail.com>
2022-05-05 22:57:51 -0400

    adding readme and video link

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const readMeTemplate = require("../readMeTemplate");
+const readMeTemplate = require("./utils/readMeTemplate");
 // TODO: Create an array of questions for user input
 const questions = [];
 const promptUser = () => {
